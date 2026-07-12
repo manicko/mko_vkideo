9ea49e6a6bc56220d80964ae0d6016551cd425d9
manicko <manicko@yandex.ru>
2026-07-12 13:34:25 +0300

    refactor(tests): remove broken test file blocking pytest collection

--- tests/test_hls_downloader_patch.py
+++
@@ -1,5 +0,0 @@
-async def mock_gather(*tasks: Any, **kwargs: Any) -> list[bool]:
-            nonlocal gather_called
-            gather_called = True
-            # Return True for each task
-            return [True] * len(tasks)
