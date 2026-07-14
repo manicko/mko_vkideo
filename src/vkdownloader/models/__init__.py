a3875873b85169bbf19fc42fb4891e513f6b6ac8
manicko <manicko@yandex.ru>
2026-07-14 16:55:30 +0300

    feat(models): add DownloadMethod and CookieSource to public exports
    Task: TASK_003_add_enum_exports

--- src/vkdownloader/models/__init__.py
+++ src/vkdownloader/models/__init__.py
@@ -1,10 +1,12 @@
 """Models package exports."""
 
 from .dtos import DownloadRequest, DownloadResult, HLSDownloadRequest
-from .enums import DownloadStatus, LogLevel, QualityEnum, StreamFormat
+from .enums import CookieSource, DownloadMethod, DownloadStatus, LogLevel, QualityEnum, StreamFormat
 from .video import DownloadProgress, Stream, Video, VideoWithStreams
 
 __all__ = [
+    "CookieSource",
+    "DownloadMethod",
     "DownloadRequest",
     "DownloadResult",
     "HLSDownloadRequest",
